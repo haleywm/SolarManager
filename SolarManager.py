6f7d531ee56a7219508a2b44076f088be3d991fd
Haley <haleywrkmn@gmail.com>
2025-10-22 20:16:14 +0800

    Made initial version of program. Needs heavy testing.

---
+++ SolarManager.py
@@ -0,0 +1,190 @@
+# Defines the solarmanager class which will help manage things
+import re
+import requests
+from datetime import datetime
+import time
+
+
+class SolarManager:
+    url_prefix = "https://openapi.growatt.com/"
+
+    def __init__(
+        self,
+        api_key: str,
+        target_percent: int,
+        start_time: str,
+        stop_time: str,
+        update_frequency: int,
+        max_delay_time: int,
+    ) -> None:
+        self.api_key: str = api_key
+        self.target_percent: int = target_percent
+        self.start_hour, self.start_minute = self._parse_time(start_time)
+        self.stop_hour, self.stop_minute = self._parse_time(stop_time)
+        self.update_frequency = update_frequency
+        self.max_delay_time = max_delay_time
+
+        # Creating a session to use for API requests
+        self.session = requests.Session()
+        self.session.headers.update({"token": self.api_key})
+
+        self.mix_sn = self.get_mix_sn()
+
+        self.battery_rule_enabled = self.get_grid_charging_enabled()
+
+    def get_mix_sn(self) -> str:
+        # In order to do anything, we first need to get the users devices serial number
+        # Step 1: List all user power plants. There will typically only be one
+        res = self.session.get(self.url_prefix + "v1/plant/list")
+        plants = res.json()["data"]["plants"]
+
+        if len(plants) == 0:
+            raise SolarError("Unable to find any power plants associated with account")
+        elif len(plants) > 1:
+            print("Warning: Multiple plants associated with account. Using first one.")
+
+        plant_id = plants[0]["plant_id"]
+        print(f"Retrieved plant id {plant_id}")
+
+        res = self.session.get(
+            self.url_prefix + "v1/device/list", params={"plant_id": plant_id}
+        )
+        devices = res.json()["data"]["devices"]
+
+        if len(devices) == 0:
+            raise SolarError("Unable to find any devices associated with plant")
+        elif len(devices) > 1:
+            print("Warning: Multiple devices associated with account. Using first one.")
+
+        mix_sn = devices[0]["device_sn"]
+        assert isinstance(mix_sn, str), "Unexpected type returned from JSON"
+        print(f"Retrieved device serial number {mix_sn}")
+
+        return mix_sn
+
+    def enable_grid_charging(self) -> None:
+        self._adjust_grid_charging(1)
+
+    def disable_grid_charging(self) -> None:
+        self._adjust_grid_charging(0)
+
+    def get_grid_charging_enabled(self) -> bool:
+        res = self.session.get(
+            self.url_prefix + "v1/device/mix/mix_data_info",
+            params={"device_sn": self.mix_sn},
+        )
+        stop_switch_status = res.json()["data"]["forcedChargeStopSwitch1"]
+        assert isinstance(stop_switch_status, int)
+
+        return stop_switch_status == 1
+
+    def get_current_charge(self) -> int:
+        res = self.session.post(
+            self.url_prefix + "v1/device/mix/mix_last_data",
+            params={"mix_sn": self.mix_sn},
+        )
+        charge = res.json()["data"]["soc"]
+        assert isinstance(charge, int), "Unexpected type returned from JSON"
+
+        # TODO: Check if data is reasonably up to date
+
+        return charge
+
+    """ def currently_active_period(self) -> bool:
+        # Check if the current period is the active period
+        # Note that this is based on the local time zone
+        current_time = datetime.now()
+
+        if self.start_hour <= self.stop_hour:
+            # Start and stop are on the same day
+
+            # Generate start and end times by replacing just the hours and smaller values
+            # That way days and months etc don't get in the way
+            start_time = current_time.replace(hour=self.start_hour, minute=self.start_minute, second=0, microsecond=0)
+            stop_time = start_time.replace(hour=self.stop_hour, minute=self.stop_minute)
+            # Only return true if current time is bigger than the start time and smaller than the end time
+            return current_time > start_time and current_time < stop_time
+        else:
+            # Start and stop are on different days
+            # Don't need this and I'm sleepy rn
+            raise NotImplementedError("Active periods across different days not yet supported") """
+
+    def run_event_loop(self) -> None:
+        print(
+            f"Starting check cycle, will check battery status every {self.update_frequency} seconds"
+        )
+        while True:
+            current_battery = self.get_current_charge()
+
+            enough_battery = current_battery >= self.target_percent
+
+            print(
+                f"Battery currently at {current_battery}%, target: {self.target_percent}%"
+            )
+
+            if enough_battery and self.battery_rule_enabled:
+                # It's time to turn off grid charging for the battery
+                print(
+                    "Battery has sufficient charge, disabling grid charging (during specified times)"
+                )
+                self.disable_grid_charging()
+                self.battery_rule_enabled = False
+                print("Grid charging disabled!")
+
+            elif not enough_battery and not self.battery_rule_enabled:
+                # The battery level is too low, enable the charging rule
+                print(
+                    "Battery has insufficient charge, enabling grid charging (during specified times)"
+                )
+                self.enable_grid_charging()
+                self.battery_rule_enabled = True
+                print("Grid charging enabled!")
+
+    def _parse_time(self, time: str) -> tuple[int, int]:
+        values = re.match(r"^(\d\d):(\d\d)$", time)
+        assert (
+            values is not None
+        ), f"Invalid time string {time}, should be in format DD:DD"
+
+        return (int(values.group(1)), int(values.group(2)))
+
+    def _adjust_grid_charging(self, active: int) -> None:
+        res = requests.post(
+            self.url_prefix + "v1/mixSet",
+            params={
+                "mix_sn": self.mix_sn,
+                "type": "mix_ac_charge_time_period",  # This means we're changing the time period on the inverter to force ac draw
+                "param1": 100,  # Charge at full capacity
+                "param2": self.target_percent,  # Cutoff charging at the target percent
+                "param3": 1,  # Enable drawing from mains
+                "param4": self.start_hour,  # Hour to start period
+                "param5": self.start_minute,  # Minute to start period
+                "param6": self.stop_hour,
+                "param7": self.stop_minute,
+                "param8": active,  # If this period should be active. Use the argument
+                "param9": 0,
+                "param10": 0,
+                "param11": 0,
+                "param12": 0,
+                "param13": 0,
+                "param14": 0,
+                "param15": 0,
+                "param16": 0,
+                "param17": 0,
+                "param18": 0,
+            },
+        )
+
+        data = res.json()
+        if data["error_code"] != 0:
+            raise SolarError(
+                f"Error {data["error_code"]} returned by API: {data["error_msg"]}"
+            )
+
+
+class SolarError(Exception):
+    pass
+
+
+class DisconnectedError(SolarError):
+    pass
---
+++ main.py
@@ -0,0 +1,32 @@
+import tomllib
+import argparse
+from pathlib import Path
+from SolarManager import SolarManager
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "config",
+        type=Path,
+        help="Location of the config file. Default: config.toml",
+        default="config.toml",
+    )
+
+    args = parser.parse_args()
+
+    with open(args.config, "rb") as f:
+        config = tomllib.load(f)
+
+    manager = SolarManager(
+        config["account"]["api_key"],
+        config["charging"]["target_percent"],
+        config["charging"]["start_time"],
+        config["charging"]["end_time"],
+        config["timing"]["update_frequency"],
+        config["timing"]["max_delay_time"],
+    )
+
+
+if __name__ == "__main__":
+    main()
